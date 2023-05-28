17155f59f2f1026dea64b5f27f375aa1bcc87fff
AdityaRajSingh1992 <105219157+AdityaRajSingh1992@users.noreply.github.com>
2023-05-28 10:57:40 +0530

    Update embedding_creator.py

--- superagi/helper/embedding_creator.py
+++ superagi/helper/embedding_creator.py
@@ -22,7 +22,7 @@ import langchain
 import openai
 from tenacity import retry, wait_random_exponential, stop_after_attempt
 
-
+class Embedding_creator_tool:
 @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
 def get_embedding(text: str, model="text-embedding-ada-002") -> list[float]:
     return openai.Embedding.create(input=[text], model=model)["data"][0]["embedding"]
