c16f3beb438532e25388db3977b6f555daf85b9d
AdityaRajSingh1992 <105219157+AdityaRajSingh1992@users.noreply.github.com>
2023-05-31 13:46:38 +0530

    Update tools.py

--- superagi/tools/embeddings_creator/tools.py
+++ superagi/tools/embeddings_creator/tools.py
@@ -9,14 +9,14 @@ from pydantic import BaseModel, Field
 
 from superagi.helper.embeddings_creator import Embedding_creator_tool
 from superagi.tools.base_tool import BaseTool
+from superagi.tools.file.read_file import ReadFileTool
+import pandas as pd
+
 
 
 
 class EmbeddingsCreatorSchema(BaseModel):
- text_input: str = Field(
-        ...,
-        description="The text to be converted into embeddings.",
-    )
+    file_name: str = Field(...,description="The text to be converted into embeddings.")
 
 class EmbeddingsCreatorTool(BaseTool):
   name = "Embeddings Generator"
@@ -24,25 +24,30 @@ class EmbeddingsCreatorTool(BaseTool):
         "A tool for converting text into embeddings"
         "Input should be a text file."
   )
-  args_schema: Type[EmbeddingsCreatorSchema] = EmbeddingsCreatorSchema
-       
-        
-  def __init__(self, model="text-embedding-ada-002"):
-   self.model = model
-        
-  def get_embedding(self, text):
-   try:
-    openai.api_key = get_config("OPENAI_API_KEY")
-    print(openai.api_key)
-    response = openai.Embedding.create(
-                input=[text],
-                engine=self.model )
-     return response['data'][0]['embedding']
-   except Exception as exception:
-    return {"error": exception} 
-        
-       
-      
+  args_schema: Type[BaseModel] = EmbeddingsCreatorSchema
+
+  def _execute(self, file_name: str):
+
+        file_content = ReadFileTool(file_name)
+        embeddings = pd.DataFrame(Embedding_creator_tool(file_content))
+        print (embeddings)
+        #embeddings.to_csv("embeddings.csv", index=False)
+        # root_dir = get_config('RESOURCES_INPUT_ROOT_DIR')
+        # final_path = file_name
+        # if root_dir is not None:
+        #     root_dir = root_dir if root_dir.startswith("/") else os.getcwd() + "/" + root_dir
+        #     root_dir = root_dir if root_dir.endswith("/") else root_dir + "/"
+        #     final_path = root_dir + file_name
+        # else:
+        #     final_path = os.getcwd() + "/" + file_name
+        #
+        # directory = os.path.dirname(final_path)
+        # os.makedirs(directory, exist_ok=True)
+        #
+        # file = open(final_path, 'r')
+        # file_content = file.read()
+        # return file_content[:1500]
+        #
 
 # class BaseEmbedding(ABC):
 
