117e0d0259168a590455049778c2b7ff4d80c42e
AdityaRajSingh1992 <105219157+AdityaRajSingh1992@users.noreply.github.com>
2023-06-09 12:31:39 +0530

    Update embeddings_creator.py

--- superagi/helper/embeddings_creator.py
+++ superagi/helper/embeddings_creator.py
@@ -2,21 +2,26 @@ import os
 import openai
 
 import openai
+
+from superagi.config.config import get_config
+
+
 #from tenacity import retry, wait_random_exponential, stop_after_attempt
 
 class Embedding_creator_tool:
 
-    def __init__(self, model="text-embedding-ada-002"):
-        self.model = model
+    # def __init__(self):
+    #     self.model
 
     #@retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
-    def get_embedding(self, text: str):
+    def get_embeddings(self, text: str):
         try:
+            model = "text-embedding-ada-002"
             openai.api_key = get_config("OPENAI_API_KEY")
             print(openai.api_key)
             response = openai.Embedding.create(
                 input=[text],
-                engine=self.model)
+                engine=model)
             return response['data'][0]['embedding']
 
         except Exception as exception:
